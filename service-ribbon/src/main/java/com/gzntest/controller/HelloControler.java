6b229ee6fe4f52657255c4d1d0b92cf0cd2b917d
gzn100 <1005595476@qq.com>
2017-12-11 01:15:49 +0800

    上传断路器

---
+++ service-ribbon/src/main/java/com/gzntest/controller/HelloControler.java
@@ -0,0 +1,23 @@
+package com.gzntest.controller;
+
+import com.gzntest.HelloService.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by GZN on 2017/12/9.
+ */
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+}
